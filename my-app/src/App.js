7e1185667f0acc25445c1ed683e8c79279f68ea9
iolumbaram <hazel77744@gmail.com>
2018-12-04 19:18:17 +0800

    allow CSS Module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import classes from './App.css';
 import  GrapesjsEditor from "./component/Library/Grapesjs.js";
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <GrapesjsEditor/>
       </div>
     );
